532313c57a065aee2225873cc260dec3baabf5ea
TS01923141 <z890709@yahoo.com.tw>
2018-03-16 16:37:14 +0800

    first commit

---
+++ app/src/main/java/com/example/cropimagetest/MainActivity.java
@@ -0,0 +1,151 @@
+package com.example.cropimagetest;
+
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+
+
+//**************************************************************************************************
+
+//Only crop feature can use
+
+//**************************************************************************************************
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Toolbar toolbar;
+    File file;
+    Uri uri;
+    Intent CamIntent, GalIntent, CropIntent;
+    final int RequestPermissionCode = 1;
+    DisplayMetrics displayMetrics;
+    int width, height;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar_main);
+        toolbar.setTitle("Crop Image");
+        setSupportActionBar(toolbar);
+
+        imageView = findViewById(R.id.imageView_main);
+        int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA);
+        if (permissionCheck == PackageManager.PERMISSION_DENIED){
+            RequestRuntimePermission();
+        }
+    }
+
+    private void RequestRuntimePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.CAMERA)){
+            Toast.makeText(this, "CAMERA permission allows us to access CAMERA app",Toast.LENGTH_SHORT).show();
+        }else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.CAMERA
+            }, RequestPermissionCode);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.btn_camera){
+            CameraOpen();
+        }else {
+            GalleryOpen();
+        }
+        return true;
+    }
+
+    private void GalleryOpen() {
+        GalIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(Intent.createChooser(GalIntent, "Select Image from Gallery"), 2);
+    }
+
+    private void CameraOpen() {
+        CamIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "testCrop"+ ".jpg");
+        uri = Uri.fromFile(file);
+        CamIntent.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+        CamIntent.putExtra("return-data", true);
+        startActivityForResult(CamIntent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0 && resultCode == RESULT_OK){
+            CropImage();
+        }else if (requestCode == 2 && resultCode == RESULT_OK){
+            if (data != null){
+                uri = data.getData();
+                CropImage();
+            }
+        }else if (requestCode == 1 && resultCode == RESULT_OK){
+            if (data != null){
+                Bundle bundle = data.getExtras();
+                Bitmap bitmap = null;
+                //若用google相簿編輯會有問題
+                if (bundle != null) {
+                    bitmap = bundle.getParcelable("data");
+                    imageView.setImageBitmap(bitmap);
+                }
+            }
+        }
+    }
+
+    private void CropImage() {
+        try{
+            CropIntent = new Intent("com.android.camera.action.CROP");
+            CropIntent.setDataAndType(uri, "image/*");
+
+            CropIntent.putExtra("crop", "true");
+            CropIntent.putExtra("outputX", 180);
+            CropIntent.putExtra("outputY", 180);
+            //調整比例
+            CropIntent.putExtra("aspectX", 3);
+            CropIntent.putExtra("aspectY", 4);
+            CropIntent.putExtra("scaleUpIfNeeded", true);
+            CropIntent.putExtra("return-data", true);
+
+            startActivityForResult(CropIntent, 1);
+        }catch (ActivityNotFoundException   e){
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case RequestPermissionCode:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission Canceled", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+}
